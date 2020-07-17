17ad31d55bcfec149ab46f0a2da4d9e6cadcbc3a
nicoale <nicoale8786@gmail.com>
2020-07-17 09:38:28 +0300

    initialised and setup API

---
+++ src/todelete.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req, res)=>{
+res.statusCode=200;
+res.setHeader = ('Content-Type', 'application/json');
+res.end = ('Nicoales Portfolio\n');
+});
+
+server.listen(port,hostname, ()=> {
+    console.log('Server running on ${port}');
+});
